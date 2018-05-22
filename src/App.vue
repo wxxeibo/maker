4dfac3d9828f9a8a238d30d294e8b24170698fbf
wxxeibo <wxxeibo@gmail.com>
2018-05-23 02:46:50 +0800

    Add error message

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
         id="orgName"
         v-model="orgName"
         type="text"
-        placeholder="chen-shy"
+        placeholder="kayi-tsj"
       ></b-form-input>
       <b-button disabled v-on:click="handleCreateOrg">Create Org</b-button>
       <b-alert show variant="danger">Currently not support create Org</b-alert>
@@ -23,10 +23,30 @@
         id="repoName"
         v-model="repoName"
         type="text"
-        placeholder="chen-shy.github.io"
+        placeholder="kayi-tsj.github.io"
       ></b-form-input>
       <b-button v-on:click="handleCreateRepo">Create Repo</b-button>
+      <p>
+        <ol>
 
+
+        <li>
+enable Github Pages
+        </li>
+<li>
+upload photos
+
+</li>
+<li>
+get link like https://XXX.github.io/attachments/69.jpg for main photo
+</li>
+</ol>
+      </p>
+    </b-form>
+
+    <h2>Step 3</h2>
+
+    <b-form inline>
       <label for="attachmentsRepoName">Attachments Repo Name</label>
       <b-form-input
         id="attachmentsRepoName"
@@ -35,34 +55,52 @@
         placeholder="attachments"
       ></b-form-input>
       <b-button v-on:click="handleCreateAttachmentsRepo">Create Attachments Repo</b-button>
-
+      <b-alert :show="showCreateAttachmentsRepoErrorMessage"
+         dismissible
+         variant="danger">
+         {{createAttachmentsRepoErrorMessage}}
+       </b-alert>
+       <b-alert v-if="createAttachmentsRepoSuccessMessage !== ''"
+          dismissible
+          variant="success">
+          <a :href="createAttachmentsRepoSuccessMessage">{{createAttachmentsRepoSuccessMessage}}</a>
+        </b-alert>
     </b-form>
+
     <img src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-const octokit = require('@octokit/rest')()
+import debugModule from "debug";
+
+import HelloWorld from "./components/HelloWorld.vue";
+import { token } from "./token.js";
+
+const debug = debugModule("maker:App");
+const octokit = require("@octokit/rest")();
 
 // oauth
 octokit.authenticate({
-  type: 'oauth',
-  token: '179b9d52abaa3e40b13cbf64af54f006a0928a37'
-})
+  type: "oauth",
+  token
+});
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
   },
   data: () => ({
-    orgName: 'chen-shy',
-    repoName: '',
-    attachmentsRepoName: 'attachments'
+    orgName: "kayi-tsj",
+    repoName: "",
+    attachmentsRepoName: "attachments",
+    showCreateAttachmentsRepoErrorMessage: false,
+    createAttachmentsRepoErrorMessage: "",
+    createAttachmentsRepoSuccessMessage: ""
   }),
-  mounted: function () {
+  mounted: function() {
     // var self = this;
     // octokit.repos
     //   .get({
@@ -75,33 +113,48 @@ export default {
   },
   methods: {
     // Currently not support creating Org
-    handleCreateOrg: function () {
+    handleCreateOrg: function() {
       // const { orgName } = this
       // octokit.org.create({ name: orgName })
       //   .then(result => {
       //     console.log('result = ', result)
       //   })
     },
-    handleCreateRepo: function () {
-      this.createRepo(this.repoName)
+    handleCreateRepo: function() {
+      this.createRepo(this.repoName);
     },
-    handleCreateAttachmentsRepo: function () {
-      this.createRepo(this.attachmentsRepoName)
+    handleCreateAttachmentsRepo: function() {
+      this.createRepo(this.attachmentsRepoName);
     },
     createRepo: function(name) {
+      debug(`Create repo: ${name} in Org: ${this.orgName}`);
       octokit.repos
-        .createForOrg({ org: this.orgName, name })
+        .createForOrg({ org: this.orgName, name, auto_init: true })
         .then(result => {
-          console.log('result = ', result)
+          debug("result =", result);
+          this.createAttachmentsRepoSuccessMessage = result.html_url;
         })
+        .catch(error => {
+          debug("error = ", error);
+          switch (error.code) {
+            case 401:
+            case 403:
+            case 422: // name already exists on this account
+              this.showCreateAttachmentsRepoErrorMessage = true;
+              this.createAttachmentsRepoErrorMessage = error.message;
+              break;
+            default:
+              break;
+          }
+        });
     }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
